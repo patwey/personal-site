3b7b17e5045144580e265b8e918536a6e9d3dc9d
Pat Wey <patrickwey@gmail.com>
2015-11-21 17:30:14 -0700

    Add to _project.html.haml
    * A project without a website does not have a website link
    * Add has_website? method to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :github_url, presence: true
+
+  def has_website?
+    self.website_url
+  end
 end
